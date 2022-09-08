3c7b144f52ee343bd926ba06aaf3f70295673541
mostafij163 <mostafijurrahman163@gmail.com>
2022-09-08 17:17:59 +0600

    Users Table Created

--- src/Components/User/Users.jsx
+++ src/Components/User/Users.jsx
@@ -1,5 +1,6 @@
-import { Col, Row } from "antd";
+import { Button, Card, Col, Row, Table } from "antd";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import User from "./User";
 
 export default function Users() {
@@ -10,13 +11,37 @@ export default function Users() {
       .then((response) => response.json())
       .then((data) => setUser(data));
   }, []);
-  return (
-    <Row>
-      {user.map((el) => (
-        <Col key={el.id} span={24}>
-          <User user={el} />
-        </Col>
-      ))}
-    </Row>
-  );
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      width: 150,
+    },
+    {
+      title: "User Name",
+      dataIndex: "username",
+      width: 150,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+    },
+    {
+      title: "Action",
+      key: "operation",
+      fixed: "right",
+      width: 100,
+      render: () => (
+        <Button shape="round">
+          <Link to={`/users/${user[0].id}`}>Details</Link>
+        </Button>
+      ),
+    },
+  ];
+  console.log(user);
+  return <Table style={{ borderRadius: "20px", margin: "50px" }} columns={columns} dataSource={user} />;
 }
