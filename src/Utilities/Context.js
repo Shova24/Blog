b3e4edc42d22bc2d66a53cc3f2b492bb75715b3a
mostafij163 <mostafijurrahman163@gmail.com>
2022-09-14 11:49:39 +0600

    async await

--- src/Utilities/Context.js
+++ src/Utilities/Context.js
@@ -5,10 +5,18 @@ const blogContext = createContext();
 
 export const BlogProvider = ({ children }) => {
   const [post, setPost] = useState([]);
-  const getPosts = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => setPost(data));
+  const getPosts = async () => {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await response.json();
+      setPost(data);
+    } catch {
+      console.log("Something went wrong while fetching data");
+    }
+
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((res) => res.json())
+    //   .then((data) => setPost(data));
   };
   return <blogContext.Provider value={{ post, setPost, getPosts }}>{children}</blogContext.Provider>;
 };
